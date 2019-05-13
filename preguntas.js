97c34d65b39d672754cf0415ac7358bf7009c8b1
Mijayo <48282657+Mijayo@users.noreply.github.com>
2019-05-13 19:35:53 +0200

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,20 @@
+var preguntas = [{
+        "pregunta": "¿Uno en ingles?",
+        "img": "../img/",
+        "respuesta1": "uno",
+        "respuesta2": "un",
+        "respuesta3": "ains",
+        "respuesta4": "one",
+        "correcta": "4"
+    },
+    {
+
+        "pregunta": "¿?",
+        "img": "../img/",
+        "respuesta1": "dos",
+        "respuesta2": "un112",
+        "respuesta3": "ains22",
+        "respuesta4": "one2",
+        "correcta": "4"
+    }
+];
